7295e9ac59f793779433a39168b9b4744a707387
Bill Hertzing <bill.hertzing@gmail.com>
2018-06-17 09:06:23 -0400

    Refactoring successful and complete

--- AceService/AceService.BaseServices.Interfaces/AceService.BaseServices.Interfaces.cs
+++ AceService/AceService.BaseServices.Interfaces/AceService.BaseServices.Interfaces.cs
@@ -1,9 +1,9 @@
 using System.Net;
-using Ace.AceService.BaseServicesModel;
+using Ace.AceService.BaseServices.Models;
 using ServiceStack;
 using ServiceStack.Configuration;
 
-namespace Ace.AceService.BaseServicesInterface {
+namespace Ace.AceService.BaseServices.Interfaces {
     public class BaseServices : Service {
         public object Any(BaseServiceIsAlive request) {
             return new IsAliveResponse { Result = $"Hello, the Name you sent me is {request.Name}" };
